8c32c6051fd39688b88ca73d23eee8ddfc393db9
Anderson Alves <20889211+andersonmalves@users.noreply.github.com>
2024-02-25 13:06:10 +0000

    add saveLogs

--- monitor.js
+++ monitor.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import fs from 'fs';
+import { log } from 'console';
 
 const repositories = [
     'nodejs/node',
@@ -6,23 +8,39 @@ const repositories = [
     'elixir-lang/elixir'
 ];
 
-async function checkNewPullRequests() {
-    for (const repo of repositories) {
-        const url = `https://api.github.com/repos/${repo}/pulls?state=open`;
-        const response = await fetch(url);
-        const pullRequests = await response.json();
-        
-        if (pullRequests.length > 0) {
-            console.log(`Novas pull requests encontradas em ${repo}:`);
-            pullRequests.forEach(pullRequest => {
-                console.log(`- ${pullRequest.title} (${pullRequest.html_url})`);
-            });
-            
-            console.log(`achamos algo novo!`);
+function saveLogs(logs) {
+    const timestamp = new Date().toISOString();
+    const logMessage = `${timestamp}: ${logs}\n`;
 
-        } else {
-            console.log(`Nenhuma nova pull request em ${repo}.`);
+    fs.appendFile('logs.txt', logMessage, (err) => {
+        if (err) {
+            console.error('Erro ao salvar logs: ', err);
         }
+    });
+}
+
+async function checkNewPullRequests() {
+    try {
+        for (const repo of repositories) {
+            const url = `https://api.github.com/repos/${repo}/pulls?state=open`;
+            const response = await fetch(url);
+            const pullRequests = await response.json();
+            
+            if (pullRequests.length > 0) {
+                console.log(`Novas pull requests encontradas em ${repo}:`);
+                pullRequests.forEach(pullRequest => {
+                    console.log(`- ${pullRequest.title} (${pullRequest.html_url})`);
+                });
+                console.log(`Novas pull requests encontradas em ${repo}`);
+                saveLogs(`Novas pull requests encontradas em ${repo}`);
+            } else {
+                console.log(`Nenhuma nova pull request em ${repo}.`);
+                saveLogs(`Nenhuma nova pull request em ${repo}`);
+            }
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        saveLogs(`Erro: ${error.message}`);
     }
 }
 
