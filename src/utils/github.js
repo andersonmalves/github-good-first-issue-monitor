28a1b304da6595ffa6f020ef24cdbd8b8fff36af
Anderson Alves <20889211+andersonmalves@users.noreply.github.com>
2024-02-25 16:41:05 +0000

    fix url

--- src/utils/github.js
+++ src/utils/github.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export async function getOpenPullRequests(repo) {
-    const url = `https://api.github.com/repos/${repo}/issues?state=open&labels=good-first-issue`;
+    const url = `https://api.github.com/repos/${repo}/issues?q=is:open+is:issue+label=good-first-issue`;
     const response = await fetch(url);
     return response.json();
 }
